fce4d59637151c01d437ce9642717cf1ab3bd10c
lucasjesusone <lucasjesusone@gmail.com>
2021-04-22 03:15:49 -0400

    Button PROPS Component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,36 +1,39 @@
-import React from 'react'
+import { Feather } from "@expo/vector-icons";
+import React from "react";
 
-import { TouchableOpacity, Text, StyleSheet, TouchableOpacityProps } from 'react-native'
-import colors from '../styles/colors';
+import {
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  TouchableOpacityProps,
+} from "react-native";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 
-interface ButtonProps extends TouchableOpacityProps{
-    title: string
+interface ButtonProps extends TouchableOpacityProps {
+  title: string;
 }
 
-export function Button ({ title, ...rest } : ButtonProps) {
-    return (
-        <TouchableOpacity 
-        activeOpacity={0.7} 
-        style={styles.button}
-        {...rest}
-        >
-          <Text style={styles.buttonText}>Alks</Text>
-        </TouchableOpacity>
-    )
+export function Button({ title, ...rest }: ButtonProps) {
+  return (
+    <TouchableOpacity activeOpacity={0.7} style={styles.button} {...rest}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    button: {
-      backgroundColor: colors.green,
-      justifyContent: "center",
-      alignItems: "center",
-      borderRadius: 16,
-      marginBottom: 10,
-      height: 56,
-      width: 56,
-    },
-    buttonText: {
-      color: colors.white,
-      fontSize: 24,
-    },
-  });
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 16,
+    marginBottom: 30,
+    height: 56,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 16,
+    fontFamily: fonts.heading,
+  },
+});
